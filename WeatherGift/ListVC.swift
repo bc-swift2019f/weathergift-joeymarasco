cf5950fee6e633eb878d1e0a7e9aac5f82eba116
Joseph Marasco <marascoj@bc.edu>
2019-10-15 14:19:23 -0400

    7.6 completed

--- WeatherGift/ListVC.swift
+++ WeatherGift/ListVC.swift
@@ -10,12 +10,41 @@ import UIKit
 
 class ListVC: UIViewController {
 
+    var locationsArray = [String]()
+    var currentPage = 0
+    
+    @IBOutlet weak var tableView: UITableView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        tableView.delegate = self as? UITableViewDelegate 
+        tableView.dataSource = self
 
         // Do any additional setup after loading the view.
     }
-    
+   
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        if segue.identifier == "ToPageVC" {
+            let destination = segue.destination as! PageVC
+            currentPage = (tableView.indexPathForSelectedRow?.row)!
+            destination.currentPage = currentPage
+            destination.locationsArray = locationsArray
+        }
+    }
 
    
+}
+
+extension ListVC: UITabBarDelegate, UITableViewDataSource {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return locationsArray.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "LocationCell", for: indexPath)
+        cell.textLabel?.text = locationsArray[indexPath.row]
+        return cell
+    }
+    
+    
 }
--- WeatherGift/PageVC.swift
+++ WeatherGift/PageVC.swift
@@ -58,13 +58,32 @@ class PageVC: UIPageViewController {
         listButton = UIButton(frame: CGRect(x: view.frame.width - barButtonWidth, y: safeHeight - barButtonHeight, width: barButtonWidth, height: barButtonHeight))
         listButton.setImage(UIImage(named: "listButton"), for: .normal)
         listButton.setImage(UIImage(named: "listButton-highlighted"), for: .highlighted)
-        listButton.addTarget(self, action: #selector(segueToLocationVC), for: .touchUpInside)
+        listButton.addTarget(self, action: #selector(segueToListVC), for: .touchUpInside)
         view.addSubview(listButton)
     }
     
-    @objc func segueToLocationVC() {
-        print("Hey, you clicked me")
+    // MARK:- Segues
+    
+    @objc func segueToListVC() {
+        performSegue(withIdentifier: "ToListVC", sender: nil)
     }
+    
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        if segue.identifier == "ToListVC" {
+            let destination = segue.destination as! ListVC
+            destination.locationsArray = locationsArray
+            destination.currentPage = currentPage
+        }
+    }
+    
+    @IBAction func unwindFromListVC(sender: UIStoryboardSegue) {
+        pageControl.numberOfPages = locationsArray.count
+        pageControl.currentPage = currentPage
+        setViewControllers([createDetailVC(forPage: currentPage)], direction: .forward, animated: false, completion: nil)
+        
+    }
+    
+    
     
     // MARK:- Create View Controller for PageViewController
     func createDetailVC(forPage page: Int) -> DetalVC {
