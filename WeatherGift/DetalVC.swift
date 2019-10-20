a642b7b617206fadab53e71bb84420f7513b1d53
Joseph Marasco <marascoj@bc.edu>
2019-10-20 14:42:15 -0400

    7.14 completed

--- WeatherGift/DetalVC.swift
+++ WeatherGift/DetalVC.swift
@@ -7,6 +7,7 @@
 //
 
 import UIKit
+import CoreLocation
 
 class DetalVC: UIViewController {
 
@@ -18,15 +19,82 @@ class DetalVC: UIViewController {
     
     var currentPage = 0
     var locationsArray = [WeatherLocation]()
-    
+    var locationManager: CLLocationManager!
+    var currentLocation: CLLocation!
+
     override func viewDidLoad() {
         super.viewDidLoad()
-        
-        locationLabel.text = locationsArray[currentPage].name
-        dateLabel.text = locationsArray[currentPage].coordinates
-       
+        updateUserInterface()
     }
     
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(animated)
+        if currentPage == 0 {
+            getLocation()
+        }
+    }
+    
+    func updateUserInterface() {
+        locationLabel.text = locationsArray[currentPage].name
+        dateLabel.text = locationsArray[currentPage].coordinates
+    }
 
   
+}
+
+extension DetalVC: CLLocationManagerDelegate {
+    func getLocation() {
+        locationManager = CLLocationManager()
+        locationManager.delegate = self
+        let status = CLLocationManager.authorizationStatus()
+        handleLocationAuthorizationStatus(status: status)
+    }
+    
+    func handleLocationAuthorizationStatus(status: CLAuthorizationStatus) {
+        switch status {
+        case .notDetermined:
+            locationManager.requestWhenInUseAuthorization()
+        case .authorizedAlways, .authorizedWhenInUse:
+            locationManager.requestLocation()
+        case .denied:
+            print("I am sorry. Can't show location. User has not authorized.")
+        case .restricted:
+            print("Access denied. Likely parental controls are restricting location services in this app.")
+        default:
+            print("ERROR")
+        }
+    }
+    func locationManager(_ manager: CLLocationManager, didChangeAuthorization status: CLAuthorizationStatus) {
+        handleLocationAuthorizationStatus(status: status)
+    }
+    
+    func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
+        let geoCoder = CLGeocoder()
+        var place = ""
+        currentLocation = locations.last
+        let currentLatitude = currentLocation.coordinate.latitude
+        let currentLongitude = currentLocation.coordinate.longitude
+        let currentCoordinates = "\(currentLatitude),\(currentLongitude)"
+        dateLabel.text = currentCoordinates
+        geoCoder.reverseGeocodeLocation(currentLocation, completionHandler:
+            {placemarks, error in
+            if placemarks != nil {
+                let placemark = placemarks?.last
+                place = (placemark?.name)!
+            } else {
+                print("Error retrieving place. Error code \(error!)")
+                place = "Unknown Weather Location"
+                
+            }
+            self.locationsArray[0].name = place
+            self.locationsArray[0].coordinates = currentCoordinates
+            self.updateUserInterface()
+            
+        })
+    }
+    
+    func locationManager(_ manager: CLLocationManager, didFailWithError error: Error) {
+        print("Failed to get user location.")
+    }
+    
 }
